e893f2f854f1ce61b1cdf6a100b824a8b7d15db8
mookid214 <mookid214@gmail.com>
2022-10-29 19:10:54 +0900

    포스트 기능구현(임시) 좋아요, 댓글 부분은 주석처리하였고 포스트서비스는 아직입니다.

---
+++ clone-backend/src/main/java/com/example/clonebackend/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.example.clonebackend.repository;
+
+import com.example.clonebackend.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+
+
+public interface PostRepository extends JpaRepository<Post, Long>{
+
+}
